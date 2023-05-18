9ccacf88eb8700a6a0cdfe7e38ec764901eb0d39
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-05-18 13:03:46 -0300

    chore: Add 3xl property in text component

--- packages/react/src/components/Text.tsx
+++ packages/react/src/components/Text.tsx
@@ -16,6 +16,7 @@ export const Text = styled('p', {
       lg: { fontSize: '$lg' },
       xl: { fontSize: '$xl' },
       '2xl': { fontSize: '$2xl' },
+      '3xl': { fontSize: '$3xl' },
       '4xl': { fontSize: '$4xl' },
       '5xl': { fontSize: '$5xl' },
       '6xl': { fontSize: '$6xl' },
