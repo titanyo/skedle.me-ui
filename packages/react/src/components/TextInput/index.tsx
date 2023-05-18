53ae1bf1300a69c2f9f05a77cb11329ec9d03367
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-05-18 13:05:41 -0300

    chore: Add leftButton, rightButton and error properties in textinput component

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,16 +1,33 @@
-import { ComponentProps, forwardRef, ElementRef } from 'react'
-import { Input, Prefix, TextInputContainer } from './styles'
+import { ComponentProps, forwardRef, ElementRef, ReactNode } from 'react'
+import {
+  Input,
+  LeftContainer,
+  Prefix,
+  RightContainer,
+  TextInputContainer,
+} from './styles'
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
+  leftButton?: ReactNode
+  rightButton?: ReactNode
+  error?: boolean
 }
 
 export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
-  ({ prefix, ...props }: TextInputProps, ref) => {
+  (
+    { prefix, leftButton, rightButton, error, ...props }: TextInputProps,
+    ref,
+  ) => {
     return (
-      <TextInputContainer>
+      <TextInputContainer error={error}>
         {!!prefix && <Prefix>{prefix}</Prefix>}
+
+        {!!leftButton && <LeftContainer>{leftButton}</LeftContainer>}
+
         <Input ref={ref} {...props} />
+
+        {!!rightButton && <RightContainer>{rightButton}</RightContainer>}
       </TextInputContainer>
     )
   },
